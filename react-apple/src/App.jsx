746e68314cc885a05779590e388d9a354cf2e3d9
Abdisa.dev <meetabdisa@gmail.com>
2025-08-19 21:34:35 +0300

    Initial commit

---
+++ react-apple/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import FirstsSec from "./components/FirstsSec/FirstsSec";
+import AlertSec from "./components/AlertSec/AlertSec";
+import SecondSec from "./components/SecondSec/SecondSec";
+import ThirdSec from "./components/ThirdSec/ThirdSec";
+import FourthSec from "./components/FourthSec/FourthSec";
+import Footer from "./components/Footer/Footer";
+import FifthSec from "./components/FifthSec/FifthSec";
+import SixthSec from "./components/SixthSec/SixthSec";
+function App() {
+  return (
+    <>
+      <Header />
+      <AlertSec />
+      <FirstsSec />
+      <SecondSec />
+      <ThirdSec />
+      <FourthSec />
+      <FifthSec />
+      <SixthSec />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ react-apple/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+// import jquery module
+import "jquery/dist/jquery.min.js";
+// import bootstrap module
+import "bootstrap/dist/css/bootstrap.min.css";
+// import bootstrap module
+import "bootstrap/dist/js/bootstrap.min.js";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
